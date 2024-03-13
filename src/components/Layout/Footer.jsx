ff4b0898d8fa0722d703a1298a09051e174eaa26
anjali <anjalichopra1994@gmail.com>
2024-03-13 23:17:23 +0100

    add: footer element & style

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div className="footer">
+      A Website by{" "}
+      <a href="https://www.linkedin.com/in/chopraanjali/">Anjali Chopra</a>
+    </div>
+  );
+}
