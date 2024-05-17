2c75bad72ee4a71e7b7122b83e1a48e415e981b7
anjali <anjalichopra1994@gmail.com>
2024-05-17 10:40:02 +0530

    add: main window pane content

--- src/components/Layout/WindowPane.jsx
+++ src/components/Layout/WindowPane.jsx
@@ -1,4 +1,7 @@
 import HorizontalRule from './HorizontalRule';
+import volumeIcon from '../../assets/volume-icon.svg';
+import arrowIcon from '../../assets/arrow-icon.svg';
+import { Link } from 'react-router-dom';
 
 export default function WindowPane() {
   return (
@@ -18,9 +21,24 @@ export default function WindowPane() {
           </div>
         </div>
       </div>
-      <div className="content-windowpane-top"></div>
+      <div className="content-windowpane-top">
+        <div className="name">Anjali Chopra</div>
+        <div className="name-guide">
+          <span>/un-juh-lee cho-pra/ </span>
+          <button className="vol-icon">
+            <img src={volumeIcon} alt="" />
+          </button>
+        </div>
+      </div>
       <HorizontalRule />
-      <div className="content-windowpane-bottom"></div>
+      <div className="content-windowpane-bottom">
+        <Link to="/contact">
+          About{' '}
+          <span className="arrow-icon">
+            <img src={arrowIcon} alt="" />
+          </span>
+        </Link>
+      </div>
     </div>
   );
 }
