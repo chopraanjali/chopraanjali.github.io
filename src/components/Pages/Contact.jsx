b389d6e4365a1d3e7d5b49e7509003faeb0a2bcf
anjali <anjalichopra1994@gmail.com>
2024-05-17 10:49:41 +0530

    fix: cleaned up Home

--- src/components/Pages/Contact.jsx
+++ src/components/Pages/Contact.jsx
@@ -1,14 +1,32 @@
+import { Link } from 'react-router-dom';
+
 export default function Contact() {
   const contact = {
-    email: "anjalichopra1994@gmail.com",
+    email: 'anjalichopra1994@gmail.com',
     phone: +4915238998631,
-    linkedin: "www.linkedin.com/in/chopraanjali",
-    github: "www.github.com/chopraanjali",
+    linkedin: 'www.linkedin.com/in/chopraanjali',
+    github: 'www.github.com/chopraanjali',
   };
   return (
-    <div>
-      <h3>Contact Information</h3>
-      <h4>{contact.email}</h4>
+    <div className="card">
+      <h1>Hi, Im Anjali!</h1>
+      <p>
+        Im a frontend developer based in Magdeburg, Germany. <br />I ensure the
+        co-existence of function and beauty in my work and love to create
+        engaging user experiences.
+      </p>
+      <div>
+        <p>
+          Tech stack: HTML5, CSS3, SASS, JavaScript, TypeScript, ReactJS,
+          NodeJS, MongoDB, Python
+        </p>
+      </div>
+      <div id="workLinkIntro">
+        Check out my work{' '}
+        <span>
+          <Link to="/projects">here</Link>
+        </span>
+      </div>
     </div>
   );
 }
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,29 +1,10 @@
-import { Link } from "react-router-dom";
-import WindowPane from "../Layout/WindowPane";
+import { Link } from 'react-router-dom';
+import WindowPane from '../Layout/WindowPane';
 export default function Home() {
   return (
     <>
       <div className="card">
         <WindowPane />
-
-        <h1>Hi, Im Anjali!</h1>
-        <p>
-          Im a frontend developer based in Magdeburg, Germany. <br />I ensure
-          the co-existence of function and beauty in my work and love to create
-          engaging user experiences.
-        </p>
-        <div>
-          <p>
-            Tech stack: HTML5, CSS3, SASS, JavaScript, TypeScript, ReactJS,
-            NodeJS, MongoDB, Python
-          </p>
-        </div>
-        <div id="workLinkIntro">
-          Check out my work{" "}
-          <span>
-            <Link to="/projects">here</Link>
-          </span>
-        </div>
       </div>
     </>
   );
