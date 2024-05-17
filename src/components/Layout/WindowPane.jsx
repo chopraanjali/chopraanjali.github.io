213086a201d9e1327dd629b2e4fb84e91f50e7af
anjali <anjalichopra1994@gmail.com>
2024-05-17 08:32:55 +0530

    add:nav buttons, horizontal rule to window pane

--- src/components/Layout/WindowPane.jsx
+++ src/components/Layout/WindowPane.jsx
@@ -1,3 +1,5 @@
+import HorizontalRule from './HorizontalRule';
+
 export default function WindowPane() {
   return (
     <div className="window-container">
@@ -8,10 +10,17 @@ export default function WindowPane() {
           <div className="quick-action-button"></div>
         </div>
         <div className="nav-buttons">
-          <div className="nav-button nav-button-left"></div>
-          <div className="nav-button nav-button-right"></div>
+          <div className="nav-button nav-button-left boxed">
+            <span>{'<'}</span>
+          </div>
+          <div className="nav-button nav-button-right boxed">
+            <span>{'>'}</span>
+          </div>
         </div>
       </div>
+      <div className="content-windowpane-top"></div>
+      <HorizontalRule />
+      <div className="content-windowpane-bottom"></div>
     </div>
   );
 }
