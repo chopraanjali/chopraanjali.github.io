d2367e5cac83e82a16fff14c85fb60faf7d98882
anjali <anjalichopra1994@gmail.com>
2024-05-18 13:09:45 +0530

    edit: renamed Contact route to About

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.scss";
-import Navbar from "./components/Layout/Navbar";
-import Footer from "./components/Layout/Footer";
-import Home from "./components/Pages/Home";
-import Contact from "./components/Pages/Contact";
-import Projects from "./components/Pages/Projects";
-import WorkExperience from "./components/Pages/WorkExperience";
-import Education from "./components/Pages/Education";
-import Art from "./components/Pages/Art";
-import WindowPane from "./components/Layout/WindowPane";
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.scss';
+import Navbar from './components/Layout/Navbar';
+import Footer from './components/Layout/Footer';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
+import Projects from './components/Pages/Projects';
+import WorkExperience from './components/Pages/WorkExperience';
+import Education from './components/Pages/Education';
+import Art from './components/Pages/Art';
+import WindowPane from './components/Layout/WindowPane';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -20,7 +20,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="" element={<Home />}></Route>
-          <Route path="contact" element={<Contact />}></Route>
+          <Route path="about" element={<About />}></Route>
           <Route path="education" element={<Education />}></Route>
           <Route path="workExperience" element={<WorkExperience />}></Route>
           <Route path="projects" element={<Projects />}></Route>
