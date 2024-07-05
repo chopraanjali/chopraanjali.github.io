6dc71483354d9a90ca0dbbe7fed9d8584c8b53f0
Anjali Chopra <anjalichopra1994@gmail.com>
2024-07-05 21:58:36 +0530

    add: toolbar moved to indep component

---
+++ src/components/Layout/Toolbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Toolbar() {
+  return (
+    <div>
+      <div className="toolbar">
+        <div className="quick-action-buttons">
+          <div className="quick-action-button"></div>
+          <div className="quick-action-button"></div>
+          <div className="quick-action-button"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Toolbar;
