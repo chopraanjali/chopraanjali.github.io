49469ccfadb2367a0b406f9768ee8fd3412029b9
Anjali Chopra <anjalichopra1994@gmail.com>
2024-07-05 22:34:00 +0530

    try: remove basename

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <>
-      <Router basename="/chopraanjali.github.io">
+      <Router>
         <Navbar />
         <Routes>
           <Route path="" element={<Home />}></Route>
